775dc94f266bd7d8b51ab1e23445d3157c23d6e0
npalleta <palleta.sp@gmail.com>
2023-07-02 13:56:12 -0300

    ajustes

--- Java_Studies/src/br/com/studies/infnet/Cliente.java
+++ Java_Studies/src/br/com/studies/infnet/Cliente.java
@@ -36,23 +36,22 @@ public class Cliente {
         return Objects.hash(nome);
     }
 
-    public String bloqueiaCompra(   Assinatura assinatura, List<Pagamento> pagamentos) {
-        String s = "";
+    public String bloqueiaCompra(Assinatura assinatura, List<Pagamento> pagamentos) {
         Optional<Pagamento> clientePagamentoAssinatura = pagamentos
-                .stream()
-                .takeWhile(pagamento -> !pagamento.getProdutos().isEmpty())
-                .filter(pagamento -> assinatura.isPagamentoAtrasado())
-                .findFirst();
+            .stream()
+            .takeWhile(pagamento -> !pagamento.getProdutos().isEmpty())
+            .filter(pagamento -> assinatura.isPagamentoAtrasado())
+            .findFirst();
 
         return clientePagamentoAssinatura.isPresent() ?
-                String.format(
-                        "%s está com restrição de compra devido a falta de pagamento de sua assinatura!%n",
-                        getNome()
-                ) :
-                String.format(
-                        "Status para %s - Compra-Ok.%n",
-                        getNome()
-                );
+            String.format(
+                "%s está com restrição de compra devido a falta de pagamento de sua assinatura!%n",
+                getNome()
+            ) :
+            String.format(
+                "Status para %s - Compra-Ok.%n",
+                getNome()
+            );
         //
     }
 }
